93b44cb915a4a667465e7c86e085d7bdd7324540
HARSHIT SHARMA <harsh007fc@gmail.com>
2021-06-13 11:45:02 +0530

    upto new task added

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,19 +1,41 @@
 import React, { Component } from 'react'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            tasks:[{id:1,text:'Task-1'},{id:2,text:'Task-2'},{id:3,text:'Task-3'}],
+            currTask:''
+        }
+    }
+    handleChange = (e) => {
+        let val = e.target.value;
+        console.log(val);
+        this.setState({currTask:val});
+    }
+    onSubmit = () =>{
+        // this.state.tasks.push({id:this.state.tasks.length + 1,text:this.state.currTask});
+        // this.state.currTask = '';
+        // console.log(this.state);
+
+        let nta = [...this.state.tasks,{id:this.state.tasks.length+1,text:this.state.currTask}];
+        this.setState({
+            tasks:nta,
+            currTask:'',
+        })
+    }
     render() {
-        const tasks = [{id:1,text:'You are the love charger'},{id:2,text:'You are the love charger'},{id:3,text:'You are the love charger'}];
         return (
             <div>
                 <div className="input-container">
-                    <input type="text" />
-                    <button>Add</button>
+                    <input onChange={this.handleChange} value={this.state.currTask} type="text" />
+                    <button onClick={this.onSubmit}>Add</button>
                 </div>
                 <div className="class-list">
                     <ul>
                        {
-                          tasks.map(task=>(
-                              <li>
+                          this.state.tasks.map(task => ( //here parantheses are used becuz we are using or returning jsx inside it
+                              <li key={task.id}>
                                   <h1>{task.text}</h1>
                                   <button>Delete</button>
                               </li>
